c18b1757991de0e9be2e2be70f713660b6efb343
marceslguima <marcelsguima@gmail.com>
2022-07-13 16:54:16 -0300

    requisito 2.5

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function login() {
-  const button = document.getElementById("entrar");
   const email = document.getElementById("email");
   const senha = document.getElementById("senha");
 
   if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    console.log("deu certo");
   }
 }
+
+const button = document.getElementById("entrar");
+button.addEventListener("click", login);
